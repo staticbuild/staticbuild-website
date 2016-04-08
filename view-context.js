adfc70038dba919156e738c8651eb474430e6846
Wayne Bloss <waynebloss@gmail.com>
2016-04-08 01:22:09 -0400

    WIP

--- view-context.js
+++ view-context.js
@@ -12,3 +12,47 @@ function readFile(filePath) {
 	return fs.readFileSync(filePath, 'utf8');
 }
 exports.readFile = readFile;
+
+function readClassAST(className) {
+	var filePath = './ast/' + className + '.json';
+	var ast = require(filePath);
+	var result = {
+		root: null,
+		fields: [],
+		functions: [],
+		static: {
+			fields: [],
+			functions: []
+		}
+	};
+	for (var i in ast) {
+		var entry = ast[i];
+		if (entry.ignore)
+			continue;
+		// Class?
+		if (
+			entry.kind === "class" && 
+			entry.name === className && 
+			entry.scope === "global"
+		) {
+			result.root = entry;
+			continue;
+		}
+		// Member?
+		if (entry.memberof !== className)
+			continue;
+		if (entry.scope === "instance") {
+			if (entry.kind === "member")
+				result.fields.push(entry);
+			else if (entry.kind === "function")
+				result.functions.push(entry);
+		} else if (entry.scope === "static") {
+			if (entry.kind === "member")
+				result.static.fields.push(entry);
+			else if (entry.kind === "function")
+				result.static.functions.push(entry);
+		}
+	}
+	return result;
+}
+exports.readClassAST = readClassAST;
